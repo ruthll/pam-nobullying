a3f46825193c317f8c48e655888027ac7d965470
RUTH LEIA DE LIMA <552119258@alunos.etec.itaqua>
2019-03-14 20:10:26 -0300

    segundo passo

--- nobullying.java
+++ nobullying.java
@@ -2,4 +2,6 @@ public class NoBullying {
  public static void main(String[] args){  
  System.out.println('Programa NoBullying');  
  System.out.println('1°- Se colocar no lugar do proximo');
+  System.out.println('2°- observar o comportamento do proximo');
+
  } } 
