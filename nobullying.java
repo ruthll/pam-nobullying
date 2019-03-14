d9a1cf1acf5aee793bc3df593bfba2fe4fdc906c
RUTH LEIA DE LIMA <552119258@alunos.etec.itaqua>
2019-03-14 20:16:42 -0300

    terceiro passo

--- nobullying.java
+++ nobullying.java
@@ -1,7 +1,7 @@
 public class NoBullying {  
- public static void main(String[] args){  
- System.out.println('Programa NoBullying');  
- System.out.println('1°- Se colocar no lugar do proximo');
-  System.out.println('2°- observar o comportamento do proximo');
-
- } } 
+	public static void main(String[] args){  
+		System.out.println("Programa NoBullying");  
+		System.out.println("1° passo - Se colocar no lugar do proximo");
+		System.out.println("2° passo - Observar o comportamento do proximo");
+		System.out.println("3° passo - Procuarar sempre conversar");
+		 } } 
