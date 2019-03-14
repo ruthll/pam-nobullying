739a94678f82fc4de58c80f9e3accd238a38a478
RUTH LEIA DE LIMA <552119258@alunos.etec.itaqua>
2019-03-14 20:22:21 -0300

    quinto passo

--- nobullying.java
+++ nobullying.java
@@ -4,6 +4,6 @@ public class NoBullying {
 		System.out.println("1° passo - Se colocar no lugar do proximo");
 		System.out.println("2° passo - Observar o comportamento do proximo");
 		System.out.println("3° passo - Procurar sempre conversar");
-		System.out.println("4° passo - Demonstrar empatia");
-		
+		System.out.println("4° passo - Demonstrar impatia");
+		System.out.println("5° passo - Não julgar");
 		 } } 
